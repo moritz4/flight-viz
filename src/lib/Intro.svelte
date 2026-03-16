a193109d8941391b4f8f7bd128506e32c93c1c1c
moritz4 <mnjhesse@gmail.com>
2026-03-16 21:30:25 +0000

    Change intro page font size

--- src/lib/Intro.svelte
+++ src/lib/Intro.svelte
@@ -9,19 +9,19 @@
 </script>
 
 <div
-  class="flex flex-col items-center gap-10 bg-[#16486A] p-20 text-white rounded-3xl font-mono"
+  class="flex flex-col items-center gap-10 bg-[#16486A] p-5 md:p-20 text-white rounded-3xl font-mono"
 >
-  <h1 class="text-4xl font-bold">UK flights</h1>
+  <h1 class="text-2xl md:text-4xl font-bold">UK flights</h1>
   <div>
     <p>
       Every day, more than 6,500 flights cross UK airspace — enough to carry
       over a million passengers.
     </p>
-    <p class="mt-5">
+    <p class="md:mt-5">
       Each follows a precise corridor: fixed airways, stacked holding patterns,
       and tightly sequenced runways.
     </p>
-    <p class="mt-5">Here's what that looked like over the last 24 hours.</p>
+    <p class="md:mt-5">Here's what that looked like over the last 24 hours.</p>
   </div>
   <button
     class="rounded-2xl border px-4 text-xl w-52 {enabled
